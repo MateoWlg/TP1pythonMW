650fa209389f8add506daf098dea15137448338f
Mateo Winling <mateo.winling@uha.fr>
2023-10-20 10:51:55 +0200

    Initial commit

---
+++ TP1ex1.py
@@ -0,0 +1,50 @@
+nom = "Winling"
+
+prenom = "Matéo"
+       
+math = 14
+       
+anglais = 10
+       
+info = 11
+       
+promotion = 2023
+       
+promotion = 2023
+       
+var = (14 +10 +11) /3
+
+moy = var
+
+print('L’étudiant', nom , prenom , 'de la promotion' , promotion , 'a une moyenne de', var)
+
+
+print (type(nom))
+       
+
+print (type(prenom))
+       
+
+print (type(math))
+       
+
+print (type(anglais))
+       
+
+print (type(info))
+       
+
+print (type(promotion))
+       
+
+print (type(moy))
+       
+
+
+
+
+
+
+
+
+
---
+++ TP1ex2.py
@@ -0,0 +1,7 @@
+jour = 20
+       
+heur = 9
+       
+minute = 30
+
+print (jour * 24 * 60 + heur * 60 + minute )
