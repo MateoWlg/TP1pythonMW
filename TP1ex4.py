24291bac11b26f3b601ce90d4440ca5885018a75
MateoWlg <147590109+MateoWlg@users.noreply.github.com>
2024-10-15 15:54:30 +0200

    Update TP1ex4.py

--- TP1ex4.py
+++ TP1ex4.py
@@ -2,4 +2,5 @@ minute_ecoule = int(input("Entrez le nombre de minutes ecoulées."))
 heure = (minute_ecoule //60 ) % 24
 jour = (minute_ecoule // 60) //24
 minute = (minute_ecoule % 60)
+jour_du_mois = (jour +1 , heure, minute)
 print(f'La date associée est Jour : {Jour},Heure {heure} {minutes} minutes écoulées est le {jour_du_mois}')
